73658f501bb9707d42ff99b315996ed89f9bdf91
sum.dv <sum.dv@geekup.vn>
2022-08-19 20:45:45 +0700

    Demo auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { StoriesModule } from './stories/stories.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [StoriesModule],
+  imports: [StoriesModule, AuthModule],
 })
 export class AppModule {}
