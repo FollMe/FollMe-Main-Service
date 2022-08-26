76de1c142f6975d9e6006e0780d8022d1cf8e286
sumgu <sum.dv@geekup.vn>
2022-08-26 23:52:39 +0700

    Update strategy oauth google

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -1,24 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-token';
-import { AuthService } from '../auth.service';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { OAuth2Client } from "google-auth-library"
+import { Request } from 'express';
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-custom";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     constructor(private authService: AuthService) {
-        super({
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        });
+        super();
     }
 
-    async validate(accessToken, refreshToken, profile, done) {
+    async validate(
+        req: Request,
+        done: (err: any, user: any, info?: any) => void
+    ): Promise<void> {
         try {
-            const user = await this.authService.findOrCreateGGAccount(profile._json);
+            const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+            const idToken = req.body.idToken;
+            if (!idToken) {
+                throw new UnauthorizedException();
+            }
+            const ticket = await client.verifyIdToken({
+                idToken,
+                audience: process.env.GOOGLE_CLIENT_ID,
+            });
+            const profile = ticket.getPayload();
+            const user = await this.authService.findOrCreateGGAccount(profile);
             done(null, user);
         } catch (err) {
             console.log(err);
-            done(err, false);
+            done(err, null);
         }
     }
 }
