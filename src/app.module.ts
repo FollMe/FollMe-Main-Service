4842db4e7985042c3ca6a43b0b671f389c15e2f3
sumgu <sum.dv@geekup.vn>
2023-08-29 23:29:20 +0700

    Log for success request

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,36 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogsModule } from './modules/blogs/blogs.module';
 import { ProfileModule } from './modules/profiles/profile.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { LogService } from './logs/logs.service';
+import { Log, LogSchema } from './logs/schemas/log.schema';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        MongooseModule.forRoot(process.env.DB_HOST),
-        StoriesModule,
-        BlogsModule,
-        AuthModule,
-        ProfileModule,
-    ],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_HOST),
+    StoriesModule,
+    BlogsModule,
+    AuthModule,
+    ProfileModule,
+    MongooseModule.forFeatureAsync([
+      {
+        name: Log.name,
+        useFactory: () => {
+          const schema = LogSchema;
+          schema.plugin(require('mongoose-slug-updater'));
+          return schema;
+        },
+      }
+    ]),
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    LogService
+  ],
 })
 export class AppModule { }
---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { LogService } from '../logs/logs.service';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(private logService: LogService) { }
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest()
+    return next
+      .handle()
+      .pipe(tap(() => {
+        this.logService.createOne({
+          location: `[${request.method}] ${request.url}`,
+          slEmail: request.user?.slEmail,
+          responseCode: context.switchToHttp().getResponse().statusCode,
+        })
+      }));
+  }
+}
