478eee971bd2d6838e0503f87353b995bf925b01
sumgu <sum.dv@geekup.vn>
2023-09-26 21:46:03 +0700

    Replace Brevo with Gmail

--- src/sharedServices/mailer.service.ts
+++ src/sharedServices/mailer.service.ts
@@ -3,22 +3,22 @@ import * as nodemailer from "nodemailer";
 
 @Injectable()
 export class MailerService {
-    private transporter: nodemailer.Transporter;
+  private transporter: nodemailer.Transporter;
 
-    constructor() {
-        // create reusable transporter object using the default SMTP transport
-        this.transporter = nodemailer.createTransport({
-            host: "smtp-relay.sendinblue.com",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: 'follme.noreply@gmail.com', // generated ethereal user
-                pass: process.env.BREVO_MASTER_PASS, // generated ethereal password
-            },
-        });
-    }
-    async sendMail(options) {
-        // send mail with defined transport object
-        await this.transporter.sendMail(options);
-    }
+  constructor() {
+    // create reusable transporter object using the default SMTP transport
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+      port: 465,
+      host: 'smtp.gmail.com'
+    });
+  }
+  async sendMail(options) {
+    // send mail with defined transport object
+    await this.transporter.sendMail(options);
+  }
 }
