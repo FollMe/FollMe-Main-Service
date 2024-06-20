5fbbbed58c73cfdf9421381eb4964b9ba12cad58
Sum Duong <sum.d@kbtgvn.tech>
2024-06-20 17:23:30 +0700

    [Format code]

--- src/sharedServices/cache.service.ts
+++ src/sharedServices/cache.service.ts
@@ -1,29 +1,29 @@
 import Redis from "ioredis";
 
 export class CacheService {
-    public redis: Redis;
+  public redis: Redis;
 
-    constructor() {
-        this.redis = new Redis({
-            port: Number(process.env.REDIS_PORT),
-            host: process.env.REDIS_HOST,
-            username: process.env.REDIS_USERNAME,
-            password: process.env.REDIS_PASSWORD,
-            maxRetriesPerRequest: 0
-        });
-    }
+  constructor() {
+    this.redis = new Redis({
+      port: Number(process.env.REDIS_PORT),
+      host: process.env.REDIS_HOST,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      maxRetriesPerRequest: 0
+    });
+  }
 
-    public get(key: string): Promise<string> {
-        if (this.redis.status != 'ready') {
-            return undefined;
-        }
-        return this.redis.get(key);
+  public get(key: string): Promise<string> {
+    if (this.redis.status != 'ready') {
+      return undefined;
     }
+    return this.redis.get(key);
+  }
 
-    public set(key: string, value: string | number | Buffer, duration: number) {
-        if (this.redis.status != 'ready') {
-            return;
-        }
-        return this.redis.set(key, value, "EX", duration);
+  public set(key: string, value: string | number | Buffer, duration: number) {
+    if (this.redis.status != 'ready') {
+      return;
     }
+    return this.redis.set(key, value, "EX", duration);
+  }
 }
